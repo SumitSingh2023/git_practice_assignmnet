b83772d9d1b9611b70a6e20f0da3e5706f9dba4d
Sumit singh <sumit700490@gmail.com>
2023-10-21 10:01:11 +0530

    prime no.code

---
+++ prime.js
@@ -0,0 +1,14 @@
+
+let num=8
+let factor=0
+for(let i=0;i<=num;i++){
+    if(num%i==1){
+        factor++
+    }
+    
+}
+if(factor==2){
+    console.log("true")
+}else{
+    console.log("false")
+}
